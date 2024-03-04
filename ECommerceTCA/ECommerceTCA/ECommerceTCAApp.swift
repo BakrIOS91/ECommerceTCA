cfeb270d39f2492c75a5bfd95b9000684eaf8b57
Bakr Mohamed <Engbakrmohamed@hotmail.com>
2024-03-04 13:56:46 +0200

    Adding Initial App Creation

---
+++ ECommerceTCA/ECommerceTCA/ECommerceTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceTCAApp.swift
+//  ECommerceTCA
+//
+//  Created by Bakr mohamed on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
