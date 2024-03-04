1d7380f57e4eb84ec807c4d244b2715a297bc9d5
Bakr Mohamed <Engbakrmohamed@hotmail.com>
2024-03-04 17:22:34 +0200

    Add String Catalog plugin

--- ECommerceTCA/ECommerceTCA/App/ECommerceTCAApp.swift
+++ ECommerceTCA/ECommerceTCA/App/ECommerceTCAApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ECommerceTCAApp: App {
     var body: some Scene {
         WindowGroup {
-            EmptyView()
+            Text(.localizable.language_Title)
         }
     }
 }
